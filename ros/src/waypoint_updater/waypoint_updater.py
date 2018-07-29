55d6f7de7b350b37b4174cfd2c951c9d7fcb7c1b
Muhammad Hassan <hassan.nust@gmail.com>
2018-07-29 21:14:28 +0200

    Detection Walkthrough

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -60,7 +60,7 @@ class WaypointUpdater(object):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
 
-        closest_idx = self.waypoint_tree.query([x, x],1)[1]
+        closest_idx = self.waypoint_tree.query([x, y],1)[1]
 
         # Check if closest is ahead or behind vehicle
         closest_coord = self.waypoints_2d[closest_idx]
